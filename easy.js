ea81d3274aa02d1bca81a2bf3c6241cf1072718d
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-21 02:14:20 +0000

    hgell

---
+++ easy.js
@@ -0,0 +1,14 @@
+var easy = {
+    color: "yellow",
+    easy: "ofcourse",
+    age: 372,
+    yeah: "yeah",
+  
+  
+meth1: function(){
+     return this.easy + " & " + this.age;
+ }
+  
+  
+    
+};
